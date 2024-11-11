cf45678f47540e26749261bd98dc3324d1aeecb1
Vichinn <vi.villarroelp@duocuc.cl>
2024-11-11 18:05:25 -0300

    dsff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
@@ -13,20 +13,24 @@ const routes: Routes = [
   },
   {
     path: 'register',
-    loadChildren: () => import('./Access/register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./Access/register/register.module').then(m => m.RegisterPageModule)
   },
   {
     path: 'perfil',
-    loadChildren: () => import('./Access/perfil/perfil.module').then( m => m.PerfilPageModule)
+    loadChildren: () => import('./Access/perfil/perfil.module').then(m => m.PerfilPageModule)
   },
   {
     path: 'inicio',
-    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
-  },  {
-    path: 'change-password',
-    loadChildren: () => import('./auth/change-password/change-password.module').then( m => m.ChangePasswordPageModule)
+    loadChildren: () => import('./inicio/inicio.module').then(m => m.InicioPageModule)
   },
-
+  {
+    path: 'change-password',
+    loadChildren: () => import('./auth/change-password/change-password.module').then(m => m.ChangePasswordPageModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminPageModule)
+  },
 ];
 
 @NgModule({
